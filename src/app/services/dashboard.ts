a9e2a9c03f5e2b215642e24057c5b63bf6676232
Mfuon Leonard <mfolee@gmail.com>
2021-02-26 22:39:02 +0300

    (feat) - added dashboard summary endpoint (#166)
    * attach current user test
    
    * attach current user test -
    
    * (test) - this commit has tests on routes
    
    * remove timeouts
    
    * test watchAll on yarn:test
    
    * this exempts failing tests
    
    * this exempts failing tests
    
    * (feat) - added dashboard summary endpoint
    
    * partial tests on dashboard feature
    
    * code climate duplication fix
    
    * code eslint fix
    
    * update husky config
    
    * unit-test command fix
    
    * update circleci.yml config
    
    * (feat) - added dashboard summary endpoint
    
    * partial tests on dashboard feature
    
    * code climate duplication fix
    
    * code eslint fix
    
    * update husky config
    
    Co-authored-by: Mfuon Leonard <leonard.oduor@apollo.co.ke>
    Co-authored-by: Francis Masha <39841583+mashafrancis@users.noreply.github.com>

---
+++ src/app/services/dashboard.ts
@@ -0,0 +1,34 @@
+import { Inject, Service } from 'typedi';
+import { AppLogger } from '../app.logger';
+
+@Service()
+export default class DashboardService {
+	private logger = new AppLogger(DashboardService.name);
+
+	constructor(
+		@Inject('userModel') private userModel: Models.UserModel,
+		@Inject('deviceModel') private deviceModel: Models.DeviceModel,
+	) {}
+
+	public async GetDeviceSummary(): Promise<any> {
+		try {
+			this.DashboardLogger('devices');
+			return await this.deviceModel.find().countDocuments();
+		} catch (e) {
+			throw e;
+		}
+	}
+
+	public async GetUsersSummary(): Promise<any> {
+		try {
+			this.DashboardLogger('users');
+			return await this.userModel.find().countDocuments();
+		} catch (e) {
+			throw e;
+		}
+	}
+
+	private DashboardLogger(msg: string) {
+		this.logger.debug(`[COUNT] Getting the number of ${msg}`);
+	}
+}
