c994b139b10e5543d4c452e4df086441ad0cdbd9
Mfuon Leonard <leonard.oduor@apollo.co.ke>
2021-02-26 11:09:34 +0300

    partial tests on dashboard feature

---
+++ src/app/api/routes/__tests__/dashboard.test.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { Container } from "typedi";
+import { AppLogger } from "../../../app.logger";
+import * as x from '../dashboard'
+
+const app = express();
+
+describe('this tests the device route', ()=>{
+  beforeEach(() => {
+    const logger = new AppLogger('Loaders');
+    Container.set('logger', logger);
+  });
+
+  it('test /pump route', async () =>  {
+    expect(app).toBeDefined();
+    x.default(app);
+  });
+});
