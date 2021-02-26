0eef751f9e2b89141c0d9c34f45be24e9bdfe7bc
Mfuon Leonard <leonard.oduor@apollo.co.ke>
2021-02-26 11:34:49 +0300

    code eslint fix

--- src/app/services/__test__/dashboard.test.ts
+++ src/app/services/__test__/dashboard.test.ts
@@ -1,24 +1,25 @@
-import { AppLogger } from "../../app.logger";
-import { Container } from "typedi";
-import DashboardService from "../dashboard";
-import User from "../../models/user";
-import Device from "../../models/device";
+import { AppLogger } from '../../app.logger';
+import { Container } from 'typedi';
+import DashboardService from '../dashboard';
+import User from '../../models/user';
+import Device from '../../models/device';
 import anything = jasmine.anything;
 
-
-const service = new DashboardService(User,Device)
+const service = new DashboardService(User, Device);
 describe('test dashboard service', () => {
-  beforeEach(() => {
-    const logger = new AppLogger('Loaders');
-    Container.set('logger', logger);
-    Container.set('DashboardService', service);
-  });
-  it('should test get device summary function', () => {
-    expect(service.GetDeviceSummary()).toBeTruthy();
-  })
+	beforeEach(() => {
+		const logger = new AppLogger('Loaders');
+		Container.set('logger', logger);
+		Container.set('DashboardService', service);
+	});
+	it('should test get device summary function', () => {
+		expect(service.GetDeviceSummary()).toBeTruthy();
+	});
 
-  it('should test get user summary function', () => {
-    expect(service.GetUsersSummary()).toBeTruthy();
-    expect(service.GetUsersSummary().catch(anything)).rejects.toThrow(expect.any(Error))
-  })
-})
+	it('should test get user summary function', () => {
+		expect(service.GetUsersSummary()).toBeTruthy();
+		expect(service.GetUsersSummary().catch(anything)).rejects.toThrow(
+			expect.any(Error),
+		);
+	});
+});
